48e710b344a829ffec6fc8e777a563ac5600ae55
Zekiyenur Aydemir <zekiyenur.aydemir@hotmail.com>
2021-05-14 22:48:52 +0300

    Create StudentManager.java
    Java Kampı 3.gün Ödev 2

---
+++ ucuncuHaftaOdev2/StudentManager.java
@@ -0,0 +1,15 @@
+package ucuncuHaftaOdev2;
+
+public class StudentManager extends UserManager {
+	public void add(String message) {
+		System.out.println("Student Eklendi :" + message);
+	}
+
+	public void update(String message) {
+		System.out.println("Student Güncellendi :" + message);
+	}
+
+	public void delete(String message) {
+		System.out.println("Student Silindi :" + message);
+	}
+}
