33c630a58ac9003bb3060a6986312c31dd041dd6
Zekiyenur Aydemir <zekiyenur.aydemir@hotmail.com>
2021-05-14 22:49:57 +0300

    Java Kampı 3.gün Ödev 2

---
+++ ucuncuHaftaOdev2/UserManager.java
@@ -0,0 +1,17 @@
+package ucuncuHaftaOdev2;
+
+public class UserManager {
+
+	public void add(String message) {
+		System.out.println("Eklendi :" + message);
+	}
+
+	public void update(String message) {
+		System.out.println("Güncellendi :" + message);
+	}
+
+	public void delete(String message) {
+		System.out.println("Silindi :" + message);
+	}
+
+}
